98bac1c136f8825a4851cc91198b881793f5ff84
Tom <120240787+QuangPhamvt@users.noreply.github.com>
2023-07-24 09:32:11 +0700

    #12 clietn post list delete fake data

--- client/src/store/post/post.state.ts
+++ client/src/store/post/post.state.ts
@@ -2,7 +2,6 @@ export interface IPostItem {
 	id: number
 	title: string
 	categories: { id: number; category: string }[]
-
 	createAt: Date
 	user: {
 		username: string
@@ -20,58 +19,8 @@ export interface IPostState {
 	post: IPost | null
 	postList: IPostItem[]
 }
-const arr = [
-	{
-		id: 1,
-		title: "Nao tomori",
-		categories: [
-			{
-				id: 1,
-				category: "van hoa",
-			},
-			{
-				id: 2,
-				category: "blog",
-			},
-		],
-		createAt: new Date(),
-		user: null,
-	},
-	{
-		id: 2,
-		title: "Nao tomori",
-		categories: [
-			{
-				id: 1,
-				category: "van hoa",
-			},
-			{
-				id: 2,
-				category: "blog",
-			},
-		],
-		createAt: new Date(),
-		user: null,
-	},
-	{
-		id: 3,
-		title: "Nao tomori",
-		categories: [
-			{
-				id: 1,
-				category: "van hoa",
-			},
-			{
-				id: 2,
-				category: "blog",
-			},
-		],
-		createAt: new Date(),
-		user: null,
-	},
-]
 const postState = (): IPostState => ({
 	post: null,
-	postList: arr,
+	postList: [],
 })
 export default postState
